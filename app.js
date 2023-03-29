50aa80ee4c4603db1704db7526774ae407df2a70
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-03-28 21:18:17 -0300

    atualizações

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var reset = document.querySelector("#reset");
+var pessoas = document.querySelector("#valor");
+var mensagemErro = document.querySelector("#mensagem-erro");
+
+reset.addEventListener('click', (evento) => {
+    if (input.value <= 0) {
+        evento.preventDefault(); // impede o envio do formulário
+        mensagemErro.textContent = 'O valor deve ser maior que 0.'; // exibe a mensagem de erro
+      } else {
+        mensagemErro.textContent = ''; // remove a mensagem de erro caso o valor seja válido
+      }
+    });
+
 
