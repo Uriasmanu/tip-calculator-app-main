a3ebb479183e52ad4e4c44056507befc9c56cfc0
Manoela Urias <maanoelaurias@gmail.com>
2023-03-28 21:47:07 -0300

    Merge branch 'main' of https://github.com/Uriasmanu/tip-calculator-app-main

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+var form = document.querySelector('form');
 var reset = document.querySelector("#reset");
 var pessoas = document.querySelector("#valor");
 var mensagemErro = document.querySelector("#mensagem-erro");
 
-reset.addEventListener('click', (evento) => {
-    if (input.value <= 0) {
+reset.addEventListener('click', function(evento)  {
+    if (pessoas.value <= 0 ) {
         evento.preventDefault(); // impede o envio do formulário
-        mensagemErro.textContent = 'O valor deve ser maior que 0.'; // exibe a mensagem de erro
+        mensagemErro.textContent = "Can't be zero"; // exibe a mensagem de erro
       } else {
         mensagemErro.textContent = ''; // remove a mensagem de erro caso o valor seja válido
       }
